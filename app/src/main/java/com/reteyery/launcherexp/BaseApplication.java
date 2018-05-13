cdeb9fb017fbb5ca6cc411980eec6364715f28bf
reteyery <729239206@qq.com>
2018-05-13 22:02:36 +0800

    添加播放器画面组件

--- app/src/main/java/com/reteyery/launcherexp/BaseApplication.java
+++ app/src/main/java/com/reteyery/launcherexp/BaseApplication.java
@@ -1,14 +1,18 @@
 package com.reteyery.launcherexp;
 
 import android.app.Application;
+import android.content.Context;
 
 import fm.qingting.qtsdk.QTSDK;
 import fm.qingting.qtsdk.play.QTPlay;
 
 public class BaseApplication extends Application {
+    public static Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        context = this;
         QTSDK.init(getApplicationContext(),"MWVlMmNhMjgtYWUzOS0xMWU2LTkyM2YtMDAxNjNlMDAyMGFk","MjZiMjEwYzEtNDQyZS0zYzk3LTk4OWYtMGNlZWEzMzgwNGMz");
         QTSDK.Debug = true;//开启日志
         QTSDK.setAuthRedirectUrl("http://qttest.qingting.fm");
