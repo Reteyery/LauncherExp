c0c42bc1bbdd926c6d5ae1ce924476c9bff5b715
liyang <729239206@qq.com>
2018-05-22 11:07:10 +0800

    activity调整

--- app/src/main/java/com/reteyery/launcherexp/MainActivity.java
+++ app/src/main/java/com/reteyery/launcherexp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.reteyery.launcherexp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,9 +17,14 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.reteyery.launcherexp.base.BaseActivity;
+import com.reteyery.launcherexp.base.BaseFragment;
 import com.reteyery.launcherexp.buss.adapter.SimpleAdapter;
+import com.reteyery.launcherexp.buss.fragment.RadioListFragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import fm.qingting.qtsdk.QTException;
@@ -29,162 +38,152 @@ import fm.qingting.qtsdk.entity.QTListEntity;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener, TabLayout.OnTabSelectedListener {
 
-    @BindView(R.id.iv_play)
-    ImageView ivPlay;
-    @BindView(R.id.vp_content)
-    ViewPager vpContent;
-    @BindView(R.id.iv_cover)
-    ImageView ivCover;
-    @BindView(R.id.iv_previous)
-    ImageView ivPrevious;
-    @BindView(R.id.iv_next)
-    ImageView ivNext;
-    @BindView(R.id.tv_title)
-    TextView tvTitle;
-    @BindView(R.id.recyclerview_detail_list)
-    RecyclerView detailRecyclerview;
     @BindView(R.id.tl_title)
-    TabLayout mTabLayout;
-    @BindView(R.id.recyclerview_radio_list)
-    RecyclerView mRecyclerview;
+    TabLayout tabLayout;
+    @BindView(R.id.vp_content)
+    ViewPager viewPager;
 
-    SimpleAdapter listAdapter, detailListAdapter;
-    int channelId;
+    int tabSize;
+    List<BaseFragment> fragmentList = new ArrayList<>();
+    List<String> titleList = new ArrayList<>();
+    RadioPagerAdapter pagerAdapter;
+    List<Channel> channelList = new ArrayList<>();
+
+    @SuppressLint("UseSparseArrays")
+    Map<Integer, Category> categoryMap = new HashMap<>();
 
     @Override
     protected View onCreateView(Bundle savedInstanceState) {
-        return View.inflate(this, R.layout.activity_main, null);
+        return  View.inflate(this, R.layout.activity_radio_main, null);
     }
 
     @Override
     protected void initData() {
-        ivPlay.setOnClickListener(view -> {
-            Intent intent = new Intent(this, RadioMainActivity.class);
-            MainActivity.this.startActivity(intent);
-//            Toast.makeText(MainActivity.this, "click", LENGTH_SHORT).show();
-        });
-        listAdapter = new SimpleAdapter<Channel>() {
-            @Override
-            public void bindData(SimpleHolder holder, Channel object) {
-                channelId = object.getId();
-                holder.mTextView.setText(object.getTitle());
-                Glide.with(holder.itemView.getContext()).load(object.getThumbs().getMediumThumb()).into(holder.mImageView);
-                holder.mLinearLayout.setOnClickListener((View v) -> {
-                    Intent intent = new Intent(v.getContext(), DetailListActivity.class);
-                    intent.putExtra(DetailListActivity.CHANNEL_ID, object.getId());
-                    v.getContext().startActivity(intent);
-//                    requestChannelDetails(channelId);
-//                    requestChannelPrograms(channelId);
-                });
-            }
-        };
+        requestCategory();
+        viewPager.addOnPageChangeListener(this);
+        pagerAdapter = new RadioPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(this);
+    }
 
-        detailListAdapter = new SimpleAdapter<ChannelProgram>() {
-            @Override
-            public void bindData(SimpleHolder holder, ChannelProgram object) {
-                holder.mTextView.setText(object.getTitle());
-                if (object.getThumbs() != null) {
-                    Glide.with(holder.itemView.getContext()).load(object.getThumbs().getMediumThumb()).into(holder.mImageView);
-                }
-                holder.mLinearLayout.setOnClickListener(v -> QTSDK.requestProgramUrl(channelId, object.getId(), (result, e) -> {
-                    if (e == null) {
-//                        url.setText("播放地址：" + result.getEditions().get(0).getUrl().get(0));
-                        ArrayList<Edition> editions = new ArrayList<>();
-                        editions.addAll(result.getEditions());
-//                        PlayerActivity.Companion.start(DetailsActivity.this,editions);
-                    } else {
-                        Toast.makeText(v.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                }));
-            }
-        };
-        mRecyclerview.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-        mRecyclerview.setAdapter(listAdapter);
-
-        detailRecyclerview.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-        detailRecyclerview.setAdapter(detailListAdapter);
-
+    private void requestCategory() {
         QTSDK.requestChannelOnDemandCategories((result, e) -> {
             if (e == null) {
                 if (result != null && result.size() > 0) {
+                    tabSize = result.size();
                     for (Category category : result) {
-                        TabLayout.Tab tab = mTabLayout.newTab();
+                        TabLayout.Tab tab = tabLayout.newTab();
                         tab.setText(category.getName());
                         tab.setTag(category);
-                        mTabLayout.addTab(tab);
+                        titleList.add(category.getName());
+                        tabLayout.addTab(tab);
+                        fragmentList.add(new RadioListFragment(category.getId()));
                     }
+
+                    for (int i = 0; i < result.size(); i++){
+                        categoryMap.put(i, result.get(i));
+                    }
+                    pagerAdapter.setFragmentList(fragmentList);
+                    pagerAdapter.notifyDataSetChanged();
                     requestList(result.get(0).getId());
                 }
             } else {
                 Toast.makeText(getBaseContext(), e.getMessage(), LENGTH_SHORT).show();
             }
         });
+    }
 
-        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+    private void requestList(int tabId) {
+        QTSDK.requestChannelOnDemandList(tabId,null,1,new QTCallback<QTListEntity<Channel>>() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                Category category = (Category) tab.getTag();
-                if (category != null) {
-                    requestList(category.getId());
+            public void done(QTListEntity<Channel> result, QTException e) {
+                if (e == null) {
+                    if (result != null) {
+                        channelList = result.getData();
+                    }
+                }else{
+                    Toast.makeText(getBaseContext(), e.getMessage(), LENGTH_SHORT).show();
                 }
             }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
         });
     }
-
 
     @Override
     protected void initOperation() {
 
     }
 
-    private void requestChannelPrograms(int channelId) {
-        QTSDK.requestChannelOnDemandProgramList(channelId, 1, new QTCallback<QTListEntity<ChannelProgram>>() {
-            @Override
-            public void done(QTListEntity<ChannelProgram> result, QTException e) {
-                if (e == null) {
-                    detailListAdapter.items = result.getData();
-                    detailListAdapter.notifyDataSetChanged();
-                }
-            }
-        });
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        Category category = (Category) tab.getTag();
+        if (category != null) {
+            requestList(category.getId());
+        }
     }
 
-    private void requestChannelDetails(int channelId) {
-        QTSDK.requestChannelOnDemand(channelId, new QTCallback<Channel>() {
-            @Override
-            public void done(Channel result, QTException e) {
-                if (e == null) {
-                    tvTitle.setText(result.getTitle());
-                    Glide.with(getBaseContext())
-                            .load(result.getThumbs().getMediumThumb())
-                            .into(ivCover);
-                }
-            }
-        });
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
     }
 
-    private void requestList(int tabId) {
-        QTSDK.requestChannelOnDemandList(tabId, null, 1, (result, e) -> {
-            if (e == null) {
-                if (result != null) {
-                    listAdapter.items = result.getData();
-                    listAdapter.notifyDataSetChanged();
-                }
-            } else {
-                Toast.makeText(getBaseContext(), e.getMessage(), LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public Map<Integer, Category> getCategoryMap() {
+        return categoryMap;
+    }
+
+    public void setCategoryMap(Map<Integer, Category> categoryMap) {
+        this.categoryMap = categoryMap;
+    }
+
+    class RadioPagerAdapter extends FragmentPagerAdapter {
+        List<BaseFragment> mFragmentList;
+        RadioPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public BaseFragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+//            RxBus.getInstance().post(new TabEvent(TAB_EVENT, position, ""));
+            return titleList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return null == fragmentList ? 0 : fragmentList.size();
+        }
+
+        public void setFragmentList(List<BaseFragment> mFragmentList) {
+            this.mFragmentList = mFragmentList;
+        }
+    }
+
 }
