3e353fb7817d16fc8d82d1d9e192ec6b23d137c3
liyang <729239206@qq.com>
2018-06-13 09:31:36 +0800

    添加搜索跳转

--- app/src/main/java/com/reteyery/launcherexp/test/adapter/MovieListAdapter.java
+++ app/src/main/java/com/reteyery/launcherexp/test/adapter/MovieListAdapter.java
@@ -1,14 +1,18 @@
 package com.reteyery.launcherexp.test.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.reteyery.launcherexp.R;
 import com.reteyery.launcherexp.test.activity.MovieListActivity;
+import com.reteyery.launcherexp.test.activity.SearchMovieActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +20,12 @@ import java.util.List;
 public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieListHolder> {
     private List<MovieListActivity.DataModel> modelList;
     private static final int IS_HEADER = 10000;
+    Context context;
     @NonNull
     @Override
     public MovieListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
+        context = parent.getContext();
         if (viewType != IS_HEADER){
             view = View.inflate(parent.getContext(), R.layout.item_movie, null);
         }else {
@@ -38,16 +44,17 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
         initData(tabRegion);
         TabLayout tabTime = view.findViewById(R.id.tbl_time);
         initData(tabTime);
+        ImageButton searchBtn = view.findViewById(R.id.ib_search);
+        searchBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(context, SearchMovieActivity.class);
+            context.startActivity(intent);
+        });
     }
 
     private void initData(TabLayout tabLayout) {
         List<String> titleList = new ArrayList<>();
         titleList.add("最近更新");
         titleList.add("最高人气");
-        titleList.add("最受好评");
-        titleList.add("最热动漫");
-        titleList.add("最热武侠");
-        titleList.add("最火偶像");
         titleList.add("最受好评");
         titleList.add("最热动漫");
         titleList.add("最热武侠");
